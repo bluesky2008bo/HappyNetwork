9cef2b2aa384db13ce9c23551164cb0de045105f
ixiaopan <i@xiaopan.me>
2013-11-27 16:02:56 +0800

    fasfsdafsadfsadf

--- src/me/xiaopan/easy/network/android/http/MethodType.java
+++
@@ -1,9 +0,0 @@
-package me.xiaopan.easy.network.android.http;
-
-/**
- * 请求方式类型
- * Created by XIAOPAN on 13-11-24.
- */
-public enum MethodType {
-    GET, POST, PUT, DELETE;
-}
---
+++ src/me/xiaopan/easy/network/android/http/annotation/Method.java
@@ -0,0 +1,20 @@
+package me.xiaopan.easy.network.android.http.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import me.xiaopan.easy.network.android.http.MethodType;
+
+/**
+ * 请求方式
+ * Created by XIAOPAN on 13-11-24.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface Method {
+    public MethodType value();
+}
