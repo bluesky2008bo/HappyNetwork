a1df48220e3aaccab027d0a39f98bd76877572d9
ixiaopan <i@xiaopan.me>
2013-12-02 17:50:12 +0800

    init

--- build/source/buildConfig/debug/me/xiaopan/easy/network/android/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package me.xiaopan.easy.network.android;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
